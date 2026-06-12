0a954c76fade5ac4dadc35694e0b597c4fc3db2f
BittuKumar25 <virat108yadav@gmail.com>
2026-06-12 10:17:57 -0700

    Update main.java

--- java project/main.java
+++ java project/main.java
@@ -1,8 +1,8 @@
-class GridItem {
+class Item {
     String itemId;
     String itemName;
     int quantity;
-    GridItem(String itemId, String itemName, int quantity) {
+    Item(String itemId, String itemName, int quantity) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.quantity = quantity;
@@ -18,11 +18,11 @@ class GridItem {
 
 class Warehouse {
 
-    GridItem[][] grid;
+    Item[][] grid;
     Warehouse(int rows, int cols) {
-        grid = new GridItem[rows][cols];
+        grid = new Item[rows][cols];
     }
-    void addItem(int row, int col, GridItem item) {
+    void addItem(int row, int col, Item item) {
         grid[row][col] = item;
     }
     void searchItem(String id) {
@@ -57,15 +57,15 @@ class Warehouse {
         }
     }
 }
-public class Main {
+public class main {
         public static void main(String[] args) {
         Warehouse warehouse = new Warehouse(5, 5);
          warehouse.addItem(0, 0,
-          new GridItem("I101", "Laptop", 10));
+          new Item("I101", "Laptop", 10));
           warehouse.addItem(1, 2,
-            new GridItem("I102", "Phone", 25));
+            new Item("I102", "Phone", 25));
         warehouse.addItem(3, 1,
-         new GridItem("I103", "Tablet", 15));
+         new Item("I103", "Tablet", 15));
         warehouse.displayWarehouse();
         warehouse.searchItem("I102");
         warehouse.searchItem("I999");
